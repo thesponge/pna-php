9d1fa070a25ee8d37e7e86ec0c856b97e1827bb6
Victor Nițu <victor@thesponge.eu>
2015-10-20 11:59:19 +0300

    More code comments

--- scraper.php
+++ scraper.php
@@ -6,6 +6,7 @@ use Goutte\Client;
 use Inline\PDF\PDF;
 
 $client = new Client();
+// Set a "real" user agent, otherwise the server will run away and hide
 $client->config['headers']['User-Agent'] = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36";
 
 
@@ -19,9 +20,10 @@ $crawler->filterXPath("//*[contains(@class, 'section-list')]/li/a")->each(functi
 $max_online = max($online_ids);
 echo "Maximum ID published on pna.ro: $max_online\n";
 
+
+// Collect the maximum collected ID
 $local_ids = [];
-$files = scandir('./pdf');
-foreach($files as $file) {
+foreach(scandir('./pdf') as $file) {
   if(preg_match("/\d+\.pdf/", $file)) {
     array_push($local_ids, trim(preg_replace('/\.pdf/', '', $file)));
   }
@@ -31,12 +33,14 @@ echo "Maximum ID stored: $max_local\n";
 
 echo ($max_online - $max_local) . " documents to save\n";
 
+// Set the ID limits for collecting the data
 if($max_local < 1) {
   $id = 1;
 } else {
   $id = $max_local + 1;
 }
 
+// Start iterating through the defined interval
 for($id; $id <= $max_online; $id++) {
   // Go to the pna.ro website
   $crawler = $client->request('GET', 'http://www.pna.ro/faces/comunicat.xhtml?id=' . $id);
@@ -44,7 +48,9 @@ for($id; $id <= $max_online; $id++) {
   // Get the latest post in this category and display the titles
   $crawler->filter('div.content-holder')->each(function ($node) {
     global $id;
+    // Manipulate the charset, avoid question marks in the output
     $html = iconv("UTF-8", "ISO-8859-1//TRANSLIT", $node->html());
+    // Create a PDF instance, then do some filesystem output
     $pdf = new PDF('/usr/bin/wkhtmltopdf');
     $pdf->loadHTML($html)
       ->encoding('UTF-8')
@@ -55,6 +61,7 @@ for($id; $id <= $max_online; $id++) {
 
 echo "Done (".($max_online - $max_local)." documents downloaded).\n";
 
+// Generate a file with all the links to downloaded files
 echo "Generating index file... ";
 $fp = fopen('index.html', 'w');
 foreach(scandir('./pdf') as $file) {
